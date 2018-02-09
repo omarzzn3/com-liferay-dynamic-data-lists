leoadb/liferay-portal

LPS-75936 Fix existing DB - change layout type to our safe type

modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/instance/lifecycle/AddDefaultSharedFormLayoutPortalInstanceLifecycleListener.java
@@ -38,6 +38,7 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.HashMap;
 import java.util.Locale;
@@ -95,15 +96,21 @@ public class AddDefaultSharedFormLayoutPortalInstanceLifecycleListener
 			group.getGroupId(), false, "/shared");
 
 		if (sharedLayout == null) {
-			addPublicLayout(company.getCompanyId(), group.getGroupId());
+			sharedLayout = addPublicLayout(
+				company.getCompanyId(), group.getGroupId());
 		}
+
+		verifyLayout(sharedLayout);
 
 		Layout privateLayout = _layoutLocalService.fetchLayoutByFriendlyURL(
 			group.getGroupId(), true, "/shared");
 
 		if (privateLayout == null) {
-			addPrivateLayout(company.getCompanyId(), group.getGroupId());
+			privateLayout = addPrivateLayout(
+				company.getCompanyId(), group.getGroupId());
 		}
+
+		verifyLayout(privateLayout);
 	}
 
 	protected Group addFormsGroup(long companyId) throws PortalException {
@@ -121,7 +128,7 @@ public class AddDefaultSharedFormLayoutPortalInstanceLifecycleListener
 			GroupConstants.FORMS_FRIENDLY_URL, false, false, true, null);
 	}
 
-	protected void addPrivateLayout(long companyId, long groupId)
+	protected Layout addPrivateLayout(long companyId, long groupId)
 		throws PortalException {
 
 		ServiceContext serviceContext = new ServiceContext();
@@ -146,9 +153,11 @@ public class AddDefaultSharedFormLayoutPortalInstanceLifecycleListener
 			serviceContext);
 
 		updateUserLayoutViewPermissionPermission(companyId, layout);
+
+		return layout;
 	}
 
-	protected void addPublicLayout(long companyId, long groupId)
+	protected Layout addPublicLayout(long companyId, long groupId)
 		throws PortalException {
 
 		ServiceContext serviceContext = new ServiceContext();
@@ -165,7 +174,7 @@ public class AddDefaultSharedFormLayoutPortalInstanceLifecycleListener
 
 		serviceContext.setUserId(defaultUserId);
 
-		_layoutLocalService.addLayout(
+		return _layoutLocalService.addLayout(
 			defaultUserId, groupId, false,
 			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "Shared",
 			StringPool.BLANK, StringPool.BLANK,
@@ -217,6 +226,19 @@ public class AddDefaultSharedFormLayoutPortalInstanceLifecycleListener
 			role.getCompanyId(), Layout.class.getName(),
 			ResourceConstants.SCOPE_GROUP, String.valueOf(layout.getGroupId()),
 			role.getRoleId(), ActionKeys.VIEW);
+	}
+
+	protected void verifyLayout(Layout layout) {
+		if (StringUtil.equals(
+				layout.getType(),
+				DDLFormPortletLayoutTypeConstants.LAYOUT_TYPE)) {
+
+			return;
+		}
+
+		layout.setType(DDLFormPortletLayoutTypeConstants.LAYOUT_TYPE);
+
+		_layoutLocalService.updateLayout(layout);
 	}
 
 	private GroupLocalService _groupLocalService;

omarzzn3/com-liferay-dynamic-data-lists

dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/internal/instance/lifecycle/AddDefaultSharedFormLayoutPortalInstanceLifecycleListener.java
@@ -32,6 +32,7 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.HashMap;
 import java.util.Locale;
@@ -89,8 +90,11 @@ public class AddDefaultSharedFormLayoutPortalInstanceLifecycleListener
 			group.getGroupId(), false, "/shared");
 
 		if (layout == null) {
-			addSharedLayout(company.getCompanyId(), group.getGroupId());
+			layout = addSharedLayout(
+				company.getCompanyId(), group.getGroupId());
 		}
+
+		verifyLayout(layout);
 	}
 
 	protected Group addFormsGroup(long companyId) throws PortalException {
@@ -108,7 +112,7 @@ public class AddDefaultSharedFormLayoutPortalInstanceLifecycleListener
 			GroupConstants.FORMS_FRIENDLY_URL, false, false, true, null);
 	}
 
-	protected void addSharedLayout(long companyId, long groupId)
+	protected Layout addSharedLayout(long companyId, long groupId)
 		throws PortalException {
 
 		ServiceContext serviceContext = new ServiceContext();
@@ -125,7 +129,7 @@ public class AddDefaultSharedFormLayoutPortalInstanceLifecycleListener
 
 		serviceContext.setUserId(defaultUserId);
 
-		_layoutLocalService.addLayout(
+		return _layoutLocalService.addLayout(
 			defaultUserId, groupId, false,
 			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "Shared",
 			StringPool.BLANK, StringPool.BLANK,
@@ -153,6 +157,19 @@ public class AddDefaultSharedFormLayoutPortalInstanceLifecycleListener
 	@Reference(unbind = "-")
 	protected void setUserLocalService(UserLocalService userLocalService) {
 		_userLocalService = userLocalService;
+	}
+
+	protected void verifyLayout(Layout layout) {
+		if (StringUtil.equals(
+				layout.getType(),
+				DDLFormPortletLayoutTypeConstants.LAYOUT_TYPE)) {
+
+			return;
+		}
+
+		layout.setType(DDLFormPortletLayoutTypeConstants.LAYOUT_TYPE);
+
+		_layoutLocalService.updateLayout(layout);
 	}
 
 	private GroupLocalService _groupLocalService;